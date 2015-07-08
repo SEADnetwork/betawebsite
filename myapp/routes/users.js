2438ca5f45d820cc3b409b1af4590114a509e961
Pieter Steyaert <steyaert.pieter@gmail.com>
2015-07-08 14:31:50 +0200

    getting raw repo data is now supported as well as github login

--- myapp/routes/users.js
+++ myapp/routes/users.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var GitHubApi = require('github');
+var https = require('https');
 
 var github = new GitHubApi({
     // required
@@ -16,49 +17,73 @@ var github = new GitHubApi({
     }
 });
 
-
-var username = "";
-var password = "";
-
-/* GET users listing. */
-router.get('/updateLocation', function(req, res, next) {
-	//console.log(req.params('test'));
-	console.log(req.query.u);
-	console.log(req.query.p);
-	res.send('mabajaat');
-});
-
-
-
-
-
-router.get('/lol', function(req, res, next) {
-
-	  github.user.getFollowingFromUser({
-    // optional:
-    // headers: {
-    //     "cookie": "blahblah"
-    // },
-    user: "subtiv"
-}, function(err, res2) {
-    console.log(JSON.stringify(res2));
-    res.send('mabajaat');
-});
-
-	  
-
-//res.send('respond with a lol');
-
-});
-
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/biomoddlondonrepo', function(req, res, next) {
 	
+	//settings
+	var username = req.query.u;
+	var password = req.query.p;
+	var codePath = 'code.xml';
+	var repoName = 'BiomoddLondon';
 
+	//aux functions
+	var authenticate = function(){
+		github.authenticate({
+			type: "basic",
+			username: username,
+			password: password
+		});	
+	}
 
+	var downloadFile = function(downloadURL, callback){
+		// send out new request to download data
+		https.get(downloadURL, function(res) {
+			var data = "";
 
+			res.on('data', function (chunk) {
+				data += chunk;
+			});
 
+			res.on("end", function() {
+				callback(null, data);
+			});
+
+		}).on("error", function() {
+			callback("error: downloading file", null);
+		});
+	}
+
+	var getDownloadUrl = function(callback){
+		github.repos.getContent({
+			user: username,
+			repo: repoName,
+			path: codePath
+		}, function(err, contentData) {
+			callback(null, contentData.download_url);
+		});
+	}
+
+	var getCode = function(callback){
+		getDownloadUrl(function(error, data){
+			if (error){
+				callback(error, null);
+			} else {
+				downloadFile(data, callback);
+			}
+		});
+	}
+
+	//start 
+	// authenticate();
+	getCode(function(error, data){
+		if (error){
+			console.log(error);
+		} else {
+			res.send(data);
+		}
+	});
 });
+
+
+
 
 module.exports = router;
